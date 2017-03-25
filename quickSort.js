4a73cbf9f53116e0a16a555151cb0553619e9c7a
Michael Tutt <tutt.michael@gmail.com>
2017-03-24 18:27:51 -0700

    quickSort attempt

---
+++ quickSort.js
@@ -0,0 +1,23 @@
+var quickSort = function(arr) {
+  if (arr.length < 2) return arr;
+  var pivot = arr.pop();
+  console.log(pivot);
+  var left = [];
+  var right = [];
+  var len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  let below = quickSort(left);
+  let above = quickSort(right);
+  return below.concat([pivot], above);
+};
+
+var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+var ans = quickSort(nums);
+console.log(ans);
