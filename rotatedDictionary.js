3899c44dceb333178fa6c885119a9b01a03f0953
Michael Tutt <tutt.michael@gmail.com>
2017-04-06 11:22:49 -0700

    rotatedDictionary binary search algorithm

---
+++ rotatedDictionary.js
@@ -0,0 +1,63 @@
+function findRotation(array, beg = 0, end = array.length - 1) {
+  let midpoint;
+  while (beg < end) {
+    console.log('New loop!');
+    console.log(beg, end);
+    midpoint = beg + Math.floor((end - beg) / 2);
+    let current = array[midpoint];
+    let prev = array[midpoint - 1];
+    let next = array[midpoint + 1];
+    
+    if (!checkAlphabetical(prev, current) && checkAlphabetical(current, next)) {
+      return midpoint;
+    }
+
+    if (!checkAlphabetical(array[beg], current)) {
+      end = midpoint - 1;
+    } else {
+      beg = midpoint + 1;
+    }
+  }
+  return null;
+}
+
+function checkAlphabetical(word1, word2) {
+  for (let i = 0; i < word1.length; i++) {
+    if (word1.charCodeAt(i) === word2.charCodeAt(i)) {
+      continue;
+    } else if (word1.charCodeAt(i) < word2.charCodeAt(i)) {
+      break;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+
+var words = [
+  'ptolemaic',
+  'retrograde',
+  'supplant',
+  'undulate',
+  'xenoepist',
+  'asymptote', // <-- rotates here!
+  'babka',
+  'banoffee',
+  'engender',
+  'karpatka',
+  'othellolagkage',
+  'pedantic'
+];
+
+var words2 = [
+  'xenoepist',
+  'asymptote', // <-- rotates here!
+  'babka',
+  'banoffee',
+  'engender',
+  'karpatka',
+  'othellolagkage',
+  'pedantic'
+];
+console.log(findRotation(words));
+console.log(findRotation(words2));
