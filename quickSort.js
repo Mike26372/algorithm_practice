c7f96a003e1cddce45f2c070076958aa1fb35ecb
Michael Tutt <tutt.michael@gmail.com>
2017-05-17 10:00:51 -0700

    add constant space implementation of quicksort

--- quickSort.js
+++ quickSort.js
@@ -17,6 +17,39 @@ var quickSort = function(arr) {
   return [...below, pivot, ...above];
 };
 
+var quickSortConstantMem = function (arr, low = 0, high = arr.length - 1) {
+  
+  if (low >= high) return;
+
+  let pivot = arr[low];
+  let leftIndex = low;
+  let rightIndex = high;
+
+  while (leftIndex < rightIndex) {
+    while (arr[leftIndex] <= pivot && leftIndex <= high) {
+      leftIndex++;
+    }
+
+    while (arr[rightIndex] > pivot) {
+      rightIndex--;
+    }
+    // if left is less than right, numbers in need of a pivot have been found
+    if (leftIndex < rightIndex) {
+      let temp = arr[leftIndex];
+      arr[leftIndex] = arr[rightIndex];
+      arr[rightIndex] = temp;
+    } else {
+      break;
+    }
+  }
+  // swap pivot value with middle value at pivot position
+  arr[low] = arr[rightIndex];
+  arr[rightIndex] = pivot;
+  quickSortConstantMem(arr, low, rightIndex - 1);
+  quickSortConstantMem(arr, rightIndex + 1, high);
+  return arr;
+};
+
 var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-var ans = quickSort(nums);
+var ans = quickSortConstantMem(nums);
 console.log(ans);
