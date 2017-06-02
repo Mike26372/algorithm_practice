b23da72034a2245d4591f70d4429102f04984130
Michael Tutt <tutt.michael@gmail.com>
2017-06-02 15:37:26 -0700

    simple in place matrix rotation

---
+++ rotateMatrix.js
@@ -0,0 +1,17 @@
+function rotateImage(a) {
+  var levels = Math.floor(a.length / 2);
+  var len = a.length;
+  
+  for (var i = 0; i < levels; i++) {
+    
+    for (var j = i; j < len - 1 - i; j++) {
+      var temp = a[i][j];
+      a[i][j] = a[len - j - 1][i];
+      a[len - j - 1][i] = a[len - i - 1][len - j - 1];
+      a[len - i - 1][len - j - 1] = a[j][len - i - 1];
+      a[j][len - i - 1] = temp;
+    }
+
+  }
+  return a;
+}
