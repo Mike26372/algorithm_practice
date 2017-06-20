e39727970898d85c33d12a73c11b8c3f036beede
Michael Tutt <tutt.michael@gmail.com>
2017-06-20 14:32:14 -0700

    test matrix spiral without return statements

--- matrixSpiral.js
+++ matrixSpiral.js
@@ -1,5 +1,5 @@
 function matrixElementsInSpiralOrder(matrix) {
-  'use strict'
+  'use strict';
   if (matrix.length < 1) return [];
   let minX = 0;
   let minY = 0;
@@ -10,24 +10,33 @@ function matrixElementsInSpiralOrder(matrix) {
   while (minX <= maxX && minY <= maxY) {
     for (let i = minX; i <= maxX; i++ ) {
       result.push(matrix[minY][i]);
-      if (result.length === total) return result;
+      // if (result.length === total) return result;
     }
     minY++;
     for (let j = minY; j <= maxY; j++) {
       result.push(matrix[j][maxX]);
-      if (result.length === total) return result;
+      // if (result.length === total) return result;
     }
     maxX--;
     for (let k = maxX; k >= minX; k--) {
       result.push(matrix[maxY][k]);
-      if (result.length === total) return result;
+      // if (result.length === total) return result;
     }
     maxY--;
     for (let l = maxY; l >= minY; l--) {
-      result.push(matrix[l][minX])
-      if (result.length === total) return result;
+      result.push(matrix[l][minX]);
+      // if (result.length === total) return result;
     }
     minX++;
   }
   return result;
 }
+
+var test = [
+  [1, 2, 3, 4],
+  [12, 13, 14, 5],
+  [11, 16, 15, 6],
+  [10, 9, 8, 7]
+];
+var ans = matrixElementsInSpiralOrder(test);
+console.log(ans);
